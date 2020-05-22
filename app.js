0d90cf54fcdd98f2009af814a7fa0417b37147f7
Santosh kumar <santosh.frev@gmail.com>
2020-05-22 16:00:40 +0530

    Assignment 4

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var promoRouter = require('./routes/promoRouter');
+var uploadRouter = require('./routes/uploadRouter');
+var favouriteRouter = require('./routes/favouriteRouter');
 
 const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
 
 const url = config.mongoUrl;
-const connect = mongoose.connect(url);
 
-const uploadRouter = require('./routes/uploadRouter');
-
-app.use('/imageUpload', uploadRouter);
+const connect = mongoose.connect(url, {
+  useMongoClient: true
+});
 
 connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
-
-
+  console.log('Connected to server!');
+}, (err) => {
+  console.log(err);
+});
 
 var app = express();
 
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  }
-  else {
+app.all('*', (req, res, nxt) => {
+  if(req.secure) {
+    return nxt();
+  } else {
     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
   }
 });
@@ -48,27 +50,58 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
+app.use(session({
+  name: 'session-id',
+  secret: 'Jesus-Loves-w@ch!ra-so-Much!!',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+// app.use(session({secret: 'SECRET'}));
 app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
 
+// authentication before resource access
+// function auth(req, res, next) {
+//   // console.log(req.signedCookies);
+//   console.log(req.session);
 
+//   // if(!req.signedCookies.user){
+//   if(!req.user){  
+//     // var authHeader = req.headers.authorization;
+//     // if(!authHeader){
+//       var err = new Error('You are not authenticated!');
+//       // res.setHeader('WWW-Authenticate', 'Basic');
+//       err.status = 403;
+//       return next(err);
+//     }
+//     else {
+//       // if(req.signedCookies.user === 'admin'){ 
+//       next();     
+//     }
+// }
+// app.use(auth);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
+app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
